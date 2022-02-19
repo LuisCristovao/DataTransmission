39693f4953b4d53e9062b511c00b0bd799f7e862
LuisCristovao <luis.cristovao92@gmail.com>
2022-02-19 20:13:48 +0000

    Update url_data_transmission.js

--- url_data_transmission/url_data_transmission.js
+++ url_data_transmission/url_data_transmission.js
@@ -1,33 +1,44 @@
-console.log("Gello")
-text_area=document.getElementById("data")
-generated_url=document.getElementById("url")
+import LZString from "./lz-string.js";
+window.createURL = createURL;
+
+console.log(LZString.compress("Gello"));
+
 var ABC = {
-    toAscii: function(bin) {
-      return bin.replace(/\s*[01]{8}\s*/g, function(bin) {
-        return String.fromCharCode(parseInt(bin, 2))
-      })
-    },
-    toBinary: function(str, spaceSeparatedOctets) {
-      return str.replace(/[\s\S]/g, function(str) {
-        str = ABC.zeroPad(str.charCodeAt().toString(2));
-        return !1 == spaceSeparatedOctets ? str : str + " "
-      })
-    },
-    zeroPad: function(num) {
-      return "00000000".slice(String(num).length) + num
-    }
-  };
-function createURL(){
-    generated_url.value=window.location.origin + window.location.pathname + "?" + encodeURI(text_area.value)
+  toAscii: function (bin) {
+    return bin.replace(/\s*[01]{8}\s*/g, function (bin) {
+      return String.fromCharCode(parseInt(bin, 2));
+    });
+  },
+  toBinary: function (str, spaceSeparatedOctets) {
+    return str.replace(/[\s\S]/g, function (str) {
+      str = ABC.zeroPad(str.charCodeAt().toString(2));
+      return !1 == spaceSeparatedOctets ? str : str + " ";
+    });
+  },
+  zeroPad: function (num) {
+    return "00000000".slice(String(num).length) + num;
+  },
+};
+export function createURL() {
+  generated_url.value =
+    window.location.origin +
+    window.location.pathname +
+    "?" +
+    LZString.compressToEncodedURIComponent(text_area.value)
 }
-function getDataFromUrl(){
-    return window.location.href.split("?")[1]
+function getDataFromUrl() {
+  return window.location.search.slice(1,window.location.search.length);
 }
 
-
-
-
-data=getDataFromUrl()
-binary_data=ABC.toBinary(data)
-decodeURIData=decodeURI(data)
-text_area.value=decodeURIData
+window.onload = () => {
+  
+  const text_area = document.getElementById("data");
+  const generated_url = document.getElementById("url");
+  window.generated_url = generated_url;
+  window.text_area = text_area;
+  const data = getDataFromUrl();
+  //const binary_data = ABC.toBinary(data);
+  //const decodeURIData = decodeURI(data);
+  const decompress_data = LZString.decompressFromEncodedURIComponent(data);
+  text_area.value = decompress_data
+};
